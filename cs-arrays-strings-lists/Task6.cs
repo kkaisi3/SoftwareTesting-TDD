1025073ed5b18667c703d3f89e6018ee97d50b7e
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-11-07 12:30:35 +0000

    add functionality to task 9

--- cs-arrays-strings-lists/Task6.cs
+++ cs-arrays-strings-lists/Task6.cs
@@ -13,40 +13,12 @@ namespace cs_arrays_strings_lists
             Array.Reverse(words);
             for (int i = 0; i < words.Length; i++)
             {
-                bool found = false;
-
-                for (int j = 0; j < searchString.Length; j++)
+                if (words[i] == searchString)
                 {
-                    if (searchString[j] != words[i][j])
-                    {
-                        break;
-                    }
-                    if (j == searchString.Length - 1)
-                    {
-                        found = true;
-                    }
-                }
 
-                if (found == true)
-                {
-                    StringBuilder sb = new StringBuilder();
-                    foreach (char c in words[i])
-                    {
-                        if (Char.IsUpper(c))
-                        {                      
-                            sb.Append(Char.ToLower(c));
-                        }
-                        else
-                        {
-                            sb.Append(Char.ToUpper(c));
-                        }
-                    }
-                    words[i] = sb.ToString();
-                    break;
                 }
             }
-            Array.Reverse(words);
-            return String.Join(" ", words);
+            return "";
         }
     }
 }
