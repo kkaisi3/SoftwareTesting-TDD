0a56760ef61847a5422b5949ec8b5d78d7053641
Jayla (they/them) <jakeclegg99@gmail.com>
2024-11-19 14:40:20 +0000

    Created LIKEDISLIKE class and tests all functioning

---
+++ source/repos/TDDCommunication/TDD&Communication/LIKEDISLIKE.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Communication
+{
+    public class LIKEDISLIKE
+    {
+        public bool IslikeButton = false;
+        public bool IsDislikeButton = false;
+
+        public string buttonspressed(string[] text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == "LIKE")
+                {
+                    IslikeButton = !IslikeButton;
+                }
+                else if (text[i] == "DISLIKE")
+                {
+                    IsDislikeButton= !IsDislikeButton;
+                }
+            }
+            if (IslikeButton && IsDislikeButton) {
+                return "NEUTRAL";
+            }
+            if (IslikeButton == true)
+            {
+
+                return "LIKE";
+            }
+            else if (IsDislikeButton == true)
+            {
+                return "DISLIKE";
+            }
+            else
+            {
+                return "NEUTRAL";
+            }
+
+            
+        }
+
+
+
+        /*
+          public string Buttonstatus(enum button)
+            {
+        
+                if(button == like)
+                {
+                    if(islikebutton == true)
+                        {
+                           return "like"
+                        }
+                    else if(islikebutton == false)
+                        {
+                            return "Neutral"
+                            
+                        }
+                }if(button == dislike)
+                {
+                    if(isdislikebutton == true)
+                        {
+                           return "dislike"
+                        }
+                    else if(islikebutton == false)
+                        {
+                            return "Neutral"
+                            
+                        }
+                }
+
+                  
+        
+        }
+
+
+        }
+         
+         
+         
+         
+         */
+
+
+    }
+}
--- source/repos/TDDCommunication/TDDTests/UnitTest1.cs
+++ source/repos/TDDCommunication/TDDTests/UnitTest1.cs
@@ -10,29 +10,29 @@ namespace TDDTests
         }
 
         [Test]
-        [TestCase(new int[] { 2, 4, 6, 7, 1}, 12)]
+        [TestCase(new int[] { 2, 4, 6, 7, 1 }, 12)]
         [TestCase(new int[] { 1 }, 0)]
-        [TestCase(new int[] { 1 , 2 }, 0)]
-        [TestCase(new int[] { 1 ,2, 3 }, 2)]
+        [TestCase(new int[] { 1, 2 }, 0)]
+        [TestCase(new int[] { 1, 2, 3 }, 2)]
         public void SumNumbersTest(int[] input, int expected)
         {
 
             SumOfNumbers testNumbers = new SumOfNumbers();
             int sumOfNumbers = testNumbers.AddNumbers(input);
-             sumOfNumbers.Should().Be(expected);
+            sumOfNumbers.Should().Be(expected);
         }
 
         [Test]
-        [TestCase(new double[] {1,2,3}, 2)]
-        [TestCase(new double[] {40,16,5,8}, 17.25)]
-        [TestCase(new double[] {2,0}, 1)]
-        [TestCase(new double[] {1}, 1)]
-        [TestCase(new double[] {0}, 0)]
-        
+        [TestCase(new double[] { 1, 2, 3 }, 2)]
+        [TestCase(new double[] { 40, 16, 5, 8 }, 17.25)]
+        [TestCase(new double[] { 2, 0 }, 1)]
+        [TestCase(new double[] { 1 }, 1)]
+        [TestCase(new double[] { 0 }, 0)]
+
         public void MeanNumberTest(double[] input, double expected)
         {
             SumOfNumbers meanTest = new SumOfNumbers();
-            
+
             double meanNums = meanTest.MeanNumber(input);
 
             meanNums.Should().Be(expected);
@@ -40,7 +40,7 @@ namespace TDDTests
 
         }
         [Test]
-        [TestCase("Northcoders", 1163 )]
+        [TestCase("Northcoders", 1163)]
         [TestCase("northcoders", 1195)]
         [TestCase("a", 97)]
         [TestCase("123", 150)]
@@ -56,6 +56,32 @@ namespace TDDTests
 
         }
 
+        [Test]
+        [TestCase(new string[]{"LIKE"}, "LIKE")]
+        [TestCase(new string[]{"DISLIKE"}, "DISLIKE")]
+        [TestCase(new string[]{"LIKE", "LIKE"}, "NEUTRAL")]
+        [TestCase(new string[]{"DISLIKE", "DISLIKE"}, "NEUTRAL")]
+
+        [TestCase(new string[]{"LIKE", "DISLIKE"}, "NEUTRAL")]
+        [TestCase(new string[]{"DISLIKE", "DISLIKE", "LIKE"}, "LIKE")]
+        [TestCase(new string[]{"LIKE", "LIKE", "LIKE"}, "LIKE")]
+
+        
+        public void TestLikeDislike(string[] input, string exprected)
+        {
+            LIKEDISLIKE liketest = new LIKEDISLIKE();
+          
+            string testAnswer = liketest.buttonspressed(input);
+
+            testAnswer.Should().Be(exprected);
+
+          
+           
+
+            
+
+
+        }
 
     }
 }
